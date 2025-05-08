7017765168d97e51c9c727efbd6831e0b3abde70
NMMRandom <naxomelgarejo.nm@gmail.com>
2025-05-08 06:18:50 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const cacheName = 'app-cache-v1';
 const assets = [
-  '/DevExCalculator/index.html',
-  '/DevExCalculator/css/styles.css',
-  '/DevExCalculator/js/devex.js',
-  '/DevExCalculator/images/icon-192.png',
-  '/DevExCalculator/images/icon-512.png',
-  '/DevExCalculator/images/logo.ico',
-  '/DevExCalculator/manifest.json'
+  '/index.html',
+  '/css/styles.css',
+  '/js/devex.js',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
+  '/images/logo.ico',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
